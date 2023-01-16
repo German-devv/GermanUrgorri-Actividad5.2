641e4624dbb6a90d6ae12dd135618482e39e651b
German-devv <germanurgorri@gmail.com>
2023-01-16 10:23:45 +0000

    16/01

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+  <!-- footer -->
+  <div class="copyright py-4 text-center text-white">
+    <div class="container">
+      <small>
+        Desarrollo web en entorno servidor - 2º DAW
+      </small>
+    </div>
+  </div>
+  <!-- footer -->
